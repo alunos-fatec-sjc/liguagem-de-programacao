69aa4ef7998e931e2ab4d06ced6730dfa44e0a8f
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-09-01 18:59:47 -0300

    Update ex3.c

--- aula-pratica-02/ex3.c
+++ aula-pratica-02/ex3.c
@@ -4,8 +4,9 @@ int conta_divisores(int n)
 {
     int cont = 0;
     for(int i = 1; i <= n; i++) {
-        if(n % i == 0)
+        if(n % i == 0){
             cont++;
+        }
     }
     return cont;
 }
