b3bcb795d7e7ad0114cacac5771b2394b106525b
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-08-31 21:51:58 -0300

    Update ex5.c

--- aula-pratica-02/ex5.c
+++ aula-pratica-02/ex5.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-int fatorial(int n);
 
 int fatorial(int n)
 {
-    if(n == 1)
+    if(n == 1) {
         return 1;
-    else
-    return n * fatorial(n - 1);
+    }
+    else {
+        return n * fatorial(n - 1);
+    }
 }
 
 int main()
@@ -14,5 +15,5 @@ int main()
     int n;
     scanf("%d", &n);
     printf("Fatorial de %d: %d", n, fatorial(n));
-    return 1;
+    return 0;
 }
