95c190aa6a6fbb336ab0e0a921159db9eeb5e373
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-08-31 22:04:20 -0300

    Update ex10.c

--- aula-pratica-03/ex10.c
+++ aula-pratica-03/ex10.c
@@ -7,17 +7,18 @@ int main()
     for(int i = 0; i < 10; i++) {
         printf("Digite um numero: ");
         scanf("%d", &num);
-        if(num == 0)
+        if(num == 0) {
             vetor[index_z--] = num;
-
-        else
+        }
+        else {
             vetor[index++] = num;
-    }
+        }
+     }
 
     for(int i  = 0; i < 10; i++) {
         printf("vetor[%d] = %d\n", i, vetor[i]);
     }
 
-    return 1;
+    return 0;
 }
 
