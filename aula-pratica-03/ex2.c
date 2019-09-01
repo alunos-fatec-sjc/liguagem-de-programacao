7bb379dbebb35442a1c66584f6c557b7ac8d79ae
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-08-31 22:06:42 -0300

    Update ex2.c

--- aula-pratica-03/ex2.c
+++ aula-pratica-03/ex2.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-int bin_to_dec(int *vetor);
 
 int bin_to_dec(int *vetor) {
     int base = 1, q = 0, result = 0, index = 4;
@@ -15,11 +14,10 @@ int bin_to_dec(int *vetor) {
 int main()
 {
     int vetor[5];
-
     for(int i = 0; i < 5; i++) {
         printf("Digite 0 ou 1: ");
         scanf("%d", &vetor[i]);
     }
-    printf("Resultado em decimal: %d", bin_to_dec(vetor));
-    return 1;
+    printf("Resultado em decimal: %d\n", bin_to_dec(vetor));
+    return 0;
 }
