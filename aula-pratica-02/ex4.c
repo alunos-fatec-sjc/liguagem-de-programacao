702be26c51d928b7bb235c3e74b87506423e6b1b
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-09-03 08:26:37 -0300

    Adicionando o enunciado e arrumando os retornos

--- aula-pratica-02/ex4.c
+++ aula-pratica-02/ex4.c
@@ -1,3 +1,14 @@
+/*
+    Escreva uma função que recebes 3 valores inteiros e positvos X, Y e Z e que verifique se esses valores
+    podem ser os comprimentos dos lados de um triângulo e, neste caso, retornar qual o tipo de triângulo formado. 
+    Para que X, Y e Z formem um triângulo é necessário que a seguinte propriedade seja satisfeita: 
+    o comprimento de cada lado de um triângulo é menor do que a soma do comprimento dos outros dois lados. 
+    O procedimento deve identificar o tipo de triângulo formado observando as seguintes definições:
+    • o Triângulo Equilátero: os comprimentos dos 3 lados são iguais.
+    • o Triângulo Isósceles: os comprimentos de 2 lados são iguais.
+    • o Triângulo Escaleno: os comprimentos dos 3 lados são diferentes.
+*/
+
 #include<stdio.h>
 
 int triangulo(int x, int y, int z)
@@ -15,9 +26,9 @@ int triangulo(int x, int y, int z)
     }
     else {
         printf("Não é um triângulo");
-        return 0;
+        return 1;
     }
-    return 1;
+    return 0;
 }
 
 int main()
