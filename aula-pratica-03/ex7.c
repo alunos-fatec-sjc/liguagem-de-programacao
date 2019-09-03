18e5a2b9648831c2e2ad2400da4e238ff1d39201
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-09-03 08:44:27 -0300

    Adicionando o enunciado

--- aula-pratica-03/ex7.c
+++ aula-pratica-03/ex7.c
@@ -1,3 +1,8 @@
+/*
+    Dado dois vetores, A (5 elementos) e B (8 elementos), 
+    faça um programa em C que imprima todos os elementos comuns aos dois vetores.
+*/
+
 #include<stdio.h>
 
 int main()
