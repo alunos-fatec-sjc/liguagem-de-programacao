9af8e9843ca4ec6c7bbe1a276100fe8a69229b7e
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-09-03 08:46:27 -0300

    Adicionando o enunciado

--- aula-pratica-03/ex8.c
+++ aula-pratica-03/ex8.c
@@ -1,3 +1,9 @@
+/*
+    Suponha um vetor N com 10 elementos e outro vetor M com 10 elementos. 
+    Faça um programa em C que calcule o produto escalar P de A por B. (
+    Isto é, P =A[1]*B[1] + A[2]*B[2] + ... A[N]+B[N]).
+*/
+
 #include<stdio.h>
 
 int main()
@@ -6,14 +12,13 @@ int main()
     for(int i = 0; i < 10; i++) {
         printf("Digite um numero para o vetor m: ");
         scanf("%d", &m[i]);
-    }
-    for(int i = 0; i < 10; i++) {
         printf("Digite um numero para o vetor n: ");
         scanf("%d", &n[i]);
     }
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 10; i++) {
         soma += (m[i] * n[i]);
+    }
 
     printf("Produto escalar de m por n: %d\n", soma);
     return 0;
