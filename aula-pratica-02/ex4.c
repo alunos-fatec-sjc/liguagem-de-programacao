44b2171f917e1ee4669af0bbf3a1e4f793267776
João Victor Cruz <50991788+joaocruz19@users.noreply.github.com>
2019-09-04 12:57:23 -0300

    Update ex4.c

--- aula-pratica-02/ex4.c
+++ aula-pratica-02/ex4.c
@@ -15,18 +15,18 @@ int triangulo(int x, int y, int z)
 {
     if((x < y + z) && (y < x + z) && (z < y + x)) {
         if (x == y && y == z) {
-        printf("O triângulo é equilátero");
+            printf("O triângulo é equilátero");
         }
         else if (x != y && y != z && x != z) {
-         printf("O triângulo é escaleno");
+             printf("O triângulo é escaleno");
         }
         else {
-         printf("O triângulo é isósceles");
+             printf("O triângulo é isósceles");
         }
     }
     else {
-        printf("Não é um triângulo");
-        return 1;
+            printf("Não é um triângulo");
+            return 1;
     }
     return 0;
 }
